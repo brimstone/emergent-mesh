4768f7ca1750bd37f288e4ff3d12bf3c4a2f6dcc
Matt <brimstone@the.narro.ws>
2013-08-16 08:03:58 -0400

    Enhancement: more to the partials enhancement

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 /*global angular:true, List_Ctrl:true, Board_Ctrl:true */
-window.app = angular.module('WebUI', ['ui.bootstrap', 'ngResource', 'graphing.directives', 'partials/list.html', 'partials/board.html', 'partials/post.html'])
+window.app = angular.module('WebUI', ['ui.bootstrap', 'ngResource', 'graphing.directives', 'partial/list.html', 'partial/board.html', 'partial/post.html'])
 .config(['$routeProvider', function($routeProvider) {
 	"use strict";
 	$routeProvider
-	.when('/board', {templateUrl: 'partials/list.html', controller: 'List_Ctrl'})
-	.when('/board/:board_id', {templateUrl: 'partials/board.html', controller: 'Board_Ctrl'})
-	.when('/post/:post_id', {templateUrl: 'partials/post.html', controller: 'Post_Ctrl'});
+	.when('/board', {templateUrl: 'partial/list.html', controller: 'List_Ctrl'})
+	.when('/board/:board_id', {templateUrl: 'partial/board.html', controller: 'Board_Ctrl'})
+	.when('/post/:post_id', {templateUrl: 'partial/post.html', controller: 'Post_Ctrl'});
 	//.otherwise({redirectTo: '/board'});
 }]);
 
