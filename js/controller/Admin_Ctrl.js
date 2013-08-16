89c98e5bd172a54b26308a53b955e474e1bc0433
Matt <brimstone@the.narro.ws>
2013-08-16 08:24:02 -0400

    Enhancement: Improved admin view vastly

--- js/controller/Admin_Ctrl.js
+++ js/controller/Admin_Ctrl.js
@@ -1,9 +1,8 @@
 window.app.controller('Admin_Ctrl', ['$scope', '$resource', '$timeout', function ($scope, $resource, $timeout) {
 	"use strict";
 	$scope.db = $resource('/db/:action');
-	$scope.pickles = 0;
 	$scope.update = function () {
-		$scope.pickles = Math.floor(Math.random()*100);
+		$scope.pickles = Math.floor(Math.random());
 		var s = $scope.db.get({action: "status"}, function() {
 			$scope.nodes = {};
 			$scope.nodekeys = [];
@@ -13,14 +12,14 @@ window.app.controller('Admin_Ctrl', ['$scope', '$resource', '$timeout', function
 				}
 				$scope.nodekeys.push(key);
 				$scope.nodes[key] = s[key];
+				// mathy things
+				$scope.nodes[key].load.percent=$scope.nodes[key].load.text / 2;
+				$scope.nodes[key].memory.text=($scope.nodes[key].memory.total - $scope.nodes[key].memory.used) + "KB";
+				$scope.nodes[key].flash.text=($scope.nodes[key].flash.total - $scope.nodes[key].flash.used) + "KB";
 				// start our flattened clients hash, indexed by mac address
 				var clients = {};
 				var mac;
 				//loop through arp entries and flip them around
-				for(mac in s[key].arp) {
-					if (!clients[mac]) { clients[mac] = {}; }
-					clients[mac].ip = s[key].arp[mac].ip;
-				}
 				for(mac in s[key].captive) {
 					if (!clients[mac]) { clients[mac] = {}; }
 					clients[mac].ip = s[key].captive[mac].ip;
@@ -37,13 +36,17 @@ window.app.controller('Admin_Ctrl', ['$scope', '$resource', '$timeout', function
 					if (!clients[mac].bitrate) { clients[mac].bitrate = 0; }
 					if (!clients[mac].time) { clients[mac].time = 0; }
 				}
+				for(mac in s[key].arp) {
+					if (!clients[mac]) { continue; }
+					clients[mac].ip = s[key].arp[mac].ip;
+				}
 				$scope.nodes[key].clients=clients;
 				delete $scope.nodes[key].arp;
 				delete $scope.nodes[key].captive;
 				delete $scope.nodes[key].station;
 			}
 		});
-		$timeout($scope.update, 5000);
+		$timeout($scope.update, 60000);
 	};
 	$scope.update();
 }]);
