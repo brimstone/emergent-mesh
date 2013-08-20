65ae3fd6f2debc7ee49533bcec7f83b91a940c57
Matt <brimstone@the.narro.ws>
2013-08-19 22:22:18 -0400

    Enhancement: Upgraded to no hashtag if it can, removed unneeded files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*global angular:true, List_Ctrl:true, Board_Ctrl:true */
 window.app = angular.module('WebUI', ['ui.bootstrap', 'ngResource', 'graphing.directives', 'templates'])
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	"use strict";
+	$locationProvider.html5Mode(true);
 	$routeProvider
 	.when('/board', {templateUrl: 'partial/list.html', controller: 'List_Ctrl'})
 	.when('/board/:board_id', {templateUrl: 'partial/board.html', controller: 'Board_Ctrl'})
