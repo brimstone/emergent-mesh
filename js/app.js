67a13caaa76ee88cc6541423e2b8caebfc359a18
Matt <brimstone@the.narro.ws>
2013-08-20 13:23:54 -0400

    Enhancement: Removed # from links, Fixed panel look for posts

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ window.app = angular.module('WebUI', ['ui.bootstrap', 'ngResource', 'graphing.di
 	$routeProvider
 	.when('/board', {templateUrl: 'partial/list.html', controller: 'List_Ctrl'})
 	.when('/board/:board_id', {templateUrl: 'partial/board.html', controller: 'Board_Ctrl'})
-	.when('/post/:post_id', {templateUrl: 'partial/post.html', controller: 'Post_Ctrl'})
+	.when('/board/post/:post_id', {templateUrl: 'partial/post.html', controller: 'Post_Ctrl'})
 	.when('/', {templateUrl: 'partial/index.html'});
 	//.otherwise({redirectTo: '/board'});
 }]);
