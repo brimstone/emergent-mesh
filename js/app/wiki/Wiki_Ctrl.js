5ef8e22cbadcd89861daa04514af534bba55c487
Matt <brimstone@the.narro.ws>
2013-08-27 08:57:53 -0400

    Enhancement: Updated wiki controller to use updated DB_Service

--- js/app/wiki/Wiki_Ctrl.js
+++ js/app/wiki/Wiki_Ctrl.js
@@ -1,5 +1,5 @@
 /* global Markdown:true */
-window.app.controller('Wiki_Ctrl', ['$scope', 'DB_Service', '$routeParams', function ($scope, DB_Service, $routeParams) {
+window.app.controller('Wiki_Ctrl', ['$scope', 'DB_Service', '$routeParams', function ($scope, DB, $routeParams) {
 	"use strict";
 
 	var page_id = $routeParams.page_id;
@@ -9,11 +9,9 @@ window.app.controller('Wiki_Ctrl', ['$scope', 'DB_Service', '$routeParams', func
 		page_id = "index";
 	}
 
-	//console.log(DB_Service);
-	//window.db_service = DB_Service;
-	$scope.page = DB_Service.get('wiki', page_id);
+	$scope.page = DB.get('wiki', page_id);
 
-	$scope.page.$promise.then(function() {
+	$scope.page.next(function() {
 		var converter1 = new Markdown.Converter();
 		var editor1 = new Markdown.Editor(converter1);
 		editor1.run();
