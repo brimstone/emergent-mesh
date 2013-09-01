a4b2416fa90cb913bc8cb615d283f11c2e43779a
Matt <brimstone@the.narro.ws>
2013-09-01 16:35:22 -0400

    Enhancement: Made board_id a number regardless of what the json says

--- js/app/board/Board_Ctrl.js
+++ js/app/board/Board_Ctrl.js
@@ -9,6 +9,7 @@ window.app.controller('Board_Ctrl', ['$scope', 'DB_Service', '$routeParams', fun
 		for (var p in $scope.posts._keys) {
 			var k=$scope.posts[$scope.posts._keys[p]];
 			k.id = $scope.posts._keys[p];
+			k.board_id /= 1;
 			if (k.board_id !== $scope.board_id) {
 				continue;
 			}
@@ -20,19 +21,4 @@ window.app.controller('Board_Ctrl', ['$scope', 'DB_Service', '$routeParams', fun
 			}
 		}
 	});
-	/*
-	$scope.allposts = DB_Service.posts;
-	$scope.allposts.$promise.then(function(){
-		for (var p in $scope.allposts) {
-			if (($scope.allposts[p].board_id + 0) === parseInt($scope.board_id, 10)) {
-				if ($scope.allposts[p].parent_id === null) {
-					var post = $scope.allposts[p];
-					post.id = p;
-					$scope.posts.push(post);
-				}
-			}
-		}
-	});
-	$scope.posts = [];
-	*/
 }]);
